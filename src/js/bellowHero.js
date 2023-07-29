a46f8a586125738fe6112ae20080e83c2edfcffd
edilan-ribeiro <eddyoneal2@hotmail.com>
2023-07-29 00:33:20 -0300

    chevron to go down added

---
+++ src/js/bellowHero.js
@@ -0,0 +1,9 @@
+const downChevronHero = document.getElementById('down-hero-chevron')
+
+downChevronHero.addEventListener('click', () =>
+    
+    document.documentElement.scrollTo ( {
+        top: 880,
+        scrollBehavior: 'smooth'
+    })
+)
