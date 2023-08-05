4d1f582c078827c40c4e841ed47f2a341f23b175
edilan-ribeiro <eddyoneal2@hotmail.com>
2023-08-05 14:59:12 -0300

    menu animations execution with display none

---
+++ src/js/actions/mobileMenuOff.js
@@ -0,0 +1,50 @@
+function mobileMenuOnOff() {
+
+    
+    let mobileCheckbox = document.getElementById("menu-hamburguer")
+    let mobileMenu = document.getElementById("menu-links")
+    
+    window.onload = function(){
+        mobileCheckbox.addEventListener("click", () => {
+
+            if (mobileCheckbox.checked && window.innerWidth <= 930) {
+                mobileMenu.style.display = "block"
+            } else {
+            setTimeout(() => {
+                mobileMenu.style.display = "none"
+                }, 440)
+            }
+        })
+    }
+      
+
+    window.addEventListener("resize", () => {
+        if (window.innerWidth > 930) {
+        mobileMenu.style.display = "flex"
+        
+        } else if (window.innerWidth <= 930  && mobileCheckbox.checked) {
+            mobileMenu.style.display = "block"
+
+        } else if (window.innerWidth <= 930  && !mobileCheckbox.checked) {
+            mobileMenu.style.display = "none"
+        }
+            else{
+                
+                mobileCheckbox.addEventListener("click", () => {
+
+                    if (mobileCheckbox.checked) {
+                        mobileMenu.style.display = "block"
+                    } else {
+                    setTimeout(() => {
+                        mobileMenu.style.display = "none"
+                        }, 440)
+                    }
+                })
+            }
+
+    })
+
+    
+}
+
+export { mobileMenuOnOff }
