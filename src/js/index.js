21f5eab2b9bc0c851822fa3122d06b317eaecf5e
edilan-ribeiro <eddyoneal2@hotmail.com>
2023-08-06 14:07:07 -0300

    no need of js for this action now

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,7 @@
-import {bellowHero} from './actions/bellowHero.js'
 import {backToTop} from './actions/backToTop.js'
 import {emailSent} from './actions/emailSent.js'
 import {mobileMenuOnOff} from './actions/mobileMenuOff.js'
 
 mobileMenuOnOff()
-bellowHero()
 backToTop()
 emailSent()
