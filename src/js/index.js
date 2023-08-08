5aacb9196613bd44a357749bc7ff2bde6f6a6a76
edilan-ribeiro <eddyoneal2@hotmail.com>
2023-08-08 13:54:50 -0300

    form removed

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,5 @@
 import {backToTop} from './actions/backToTop.js'
-import {emailSent} from './actions/emailSent.js'
 import {mobileMenuOnOff} from './actions/mobileMenuOff.js'
 
 mobileMenuOnOff()
 backToTop()
-emailSent()
