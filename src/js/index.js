b42ee12e6dc91465f2cc02b9289ed1335a8c546e
edilan-ribeiro <eddyoneal2@hotmail.com>
2023-08-09 04:15:35 -0300

    img slideshow turned off, incompatible with menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import {backToTop} from './actions/backToTop.js'
 import {mobileMenuOnOff} from './actions/mobileMenuOff.js'
-import {backgroundSlideshow} from './actions/bgChange.js'
+// import {backgroundSlideshow} from './actions/bgChange.js'
 
 mobileMenuOnOff()
 backToTop()
-backgroundSlideshow()
+// backgroundSlideshow()
