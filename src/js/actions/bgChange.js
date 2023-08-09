2108e92e72a683bf96d104fe5268cb16db68c739
edilan-ribeiro <eddyoneal2@hotmail.com>
2023-08-08 23:28:58 -0300

    background slider added

---
+++ src/js/actions/bgChange.js
@@ -0,0 +1,29 @@
+function backgroundSlideshow() {
+
+    let images = [
+        '../../src/images/backgrounds/clouds1.jpg',
+        '../../src/images/backgrounds/space1.jpg',
+        '../../src/images/backgrounds/fractal1.jpg',
+        '../../src/images/backgrounds/sky1.jpg',
+        '../../src/images/backgrounds/space2.jpg',
+                    
+    ]
+
+    let imgChangeTime = 3000
+    let heroSection = document.getElementById('hero')
+
+    let currentIndex = 0;
+
+    function bgChange() {
+        heroSection.style.backgroundImage = `url(${images[currentIndex]})`
+
+        currentIndex = (currentIndex + 1) % images.length
+
+        setTimeout(bgChange, imgChangeTime)
+    }
+
+    window.onload = bgChange;
+
+}
+
+export {backgroundSlideshow}
