3d529be4328f3589a532eaaef9fe1aed59e70b54
edilan-ribeiro <eddyoneal2@hotmail.com>
2023-08-10 04:36:08 -0300

    unused code removed

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,5 @@
 import {backToTop} from './actions/backToTop.js'
 import {mobileMenuOnOff} from './actions/mobileMenuOff.js'
-// import {backgroundSlideshow} from './actions/bgChange.js'
 
 mobileMenuOnOff()
 backToTop()
-// backgroundSlideshow()
