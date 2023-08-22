aa613254e09d02a02bc970443f81ab4d02e0c565
edilan-ribeiro <eddyoneal2@hotmail.com>
2023-08-22 13:13:47 -0300

    section transition added

---
+++ src/js/actions/section-transition.js
@@ -0,0 +1,44 @@
+function sectionTransition() {
+    const topSides = document.querySelectorAll('.top-side')
+    const rightSides = document.querySelectorAll('.right-side')
+    const leftSides = document.querySelectorAll('.left-side')
+    const projects = document.querySelectorAll('.project')
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+
+            if (entry.isIntersecting) {
+                let animationClass = ''
+
+                if (entry.target.classList.contains('top-side')) {
+                    animationClass = 'slide-in-top'
+                } else if (entry.target.classList.contains('right-side')) {
+                    animationClass = 'slide-in-right'
+                } else if (entry.target.classList.contains('left-side')) {
+                    animationClass = 'slide-in-left'
+                } else if (entry.target.classList.contains('project')) {
+                    animationClass = 'roll-in-blurred-top'
+                }
+
+                entry.target.classList.add(animationClass)
+
+                setTimeout(() => {
+                    entry.target.classList.remove('hide')
+                }, 200)
+
+                entry.target.addEventListener('animationend', () => {
+                    entry.target.classList.remove(animationClass)
+                    observer.unobserve(entry.target)
+                })
+            }
+        })
+    })
+
+    topSides.forEach(topSide => observer.observe(topSide))
+    rightSides.forEach(rightSide => observer.observe(rightSide))
+    leftSides.forEach(leftSide => observer.observe(leftSide))
+    projects.forEach(project => observer.observe(project))
+
+}
+
+export {sectionTransition}
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 import {backToTop} from './actions/backToTop.js'
 import {mobileMenuOnOff} from './actions/mobileMenuOff.js'
+import { sectionTransition } from './actions/section-transition.js'
 
 mobileMenuOnOff()
 backToTop()
+sectionTransition()
