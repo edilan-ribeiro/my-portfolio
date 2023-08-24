6803bb9df0ca19aa623dab8cfebf5af75f189d61
edilan-ribeiro <eddyoneal2@hotmail.com>
2023-08-24 14:20:00 -0300

    removed show more button

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,7 @@
 import {backToTop} from './actions/backToTop.js'
 import {mobileMenuOnOff} from './actions/mobileMenuOff.js'
 import { sectionTransition } from './actions/sectionTransition.js'
-import {showMore} from './actions/showMore.js'
 
 mobileMenuOnOff()
 backToTop()
 sectionTransition()
-showMore()
