b03d212b9cd0b5ed4f5d06cfa74183797f224906
edilan-ribeiro <eddyoneal2@hotmail.com>
2024-03-04 19:01:19 -0300

    chore: moved old portfolio to branch "legacy" and initialized nextjs

--- src/js/main.js
+++
@@ -1,7 +0,0 @@
-import {backToTop} from './actions/backToTop.js'
-import {mobileMenuOnOff} from './actions/mobileMenuOff.js'
-import { sectionTransition } from './actions/sectionTransition.js'
-
-mobileMenuOnOff()
-backToTop()
-sectionTransition()
