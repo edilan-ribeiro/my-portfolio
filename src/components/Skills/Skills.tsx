12b8d5b076d7f49b2b6e22290855e6c63874a3c8
edilan-ribeiro <eddyoneal2@hotmail.com>
2024-03-07 21:51:18 -0300

    feat: created initial skills component

---
+++ src/components/Skills/Skills.tsx
@@ -0,0 +1,55 @@
+import skillData from '@/data/skills.json'
+import skillsStyles from './skills.module.scss'
+import { SectionTitle } from '../SectionTitle/SectionTitle'
+import { FaCss3Alt, FaGitAlt, FaHtml5, FaNodeJs, FaReact, FaSass } from 'react-icons/fa'
+import { RiJavascriptFill } from 'react-icons/ri'
+import { SiExpress, SiNextdotjs, SiTailwindcss } from 'react-icons/si'
+import { BiLogoTypescript, BiLogoPostgresql } from 'react-icons/bi'
+
+export const Skills = () => {
+	interface icon {
+		[key: string]: JSX.Element
+	}
+
+	const icons: icon = {
+		HTML: <FaHtml5 />,
+		CSS: <FaCss3Alt />,
+		JavaScript: <RiJavascriptFill />,
+		TypeScript: <BiLogoTypescript />,
+		React: <FaReact />,
+		'Next.js': <SiNextdotjs />,
+		Sass: <FaSass />,
+		Tailwind: <SiTailwindcss />,
+		'Git/GitHub': <FaGitAlt />,
+		PostgreSQL: <BiLogoPostgresql />,
+		'Node.js': <FaNodeJs />,
+		Express: <SiExpress />,
+	}
+
+	return (
+		<div className={skillsStyles.contentContainer}>
+			
+			<SectionTitle sectionTitle='Habilidades' />
+
+			<div className={skillsStyles.skillsContainer}>
+
+				{skillData.map((skill) => (
+					<div key={skill.name} className={skillsStyles.skillCard}>
+						
+						<div className={skillsStyles[skill.color]}>
+
+							<div className={skillsStyles.iconContainer}>
+								{icons[skill.name]}
+							</div>
+
+							<h3>{skill.name}</h3>
+						</div>
+
+						<p>{skill.description}</p>
+					</div>
+				))}
+
+			</div>
+		</div>
+	)
+}
