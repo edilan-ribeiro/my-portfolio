d70baee48d3b78d31b675a3825dcab92b446f270
edilan-ribeiro <eddyoneal2@hotmail.com>
2024-03-08 22:04:57 -0300

    chore: ensure correct data type is returned and in order from newer to older

--- src/services/fetchProjectData.ts
+++ src/services/fetchProjectData.ts
@@ -1,20 +1,39 @@
-export async function fetchProjectData() {
-	let projectData
+interface projectData {
+	name: string
+	description: string
+	topics: string[]
+	homepage: string
+	html_url: string
+	id: number
+	created_at: string
+}
+
+export async function fetchProjectData(): Promise<projectData[]> {
+	let projectData: projectData[] = []
 
 	try {
 		const url = `https://api.github.com/users/edilan-ribeiro/repos`
 
 		const dataCacheTime = 60 * 60 * 24 * 25 // 25 days to revalidate cache
 
-		const getData = await fetch(url, { next: { revalidate: 10 } })
+		const getData = await fetch(url, { next: { revalidate: dataCacheTime } })
 
 		projectData = await getData.json()
-
 	} catch (error) {
 		console.log(`Ocorreu um erro ${error}`)
 	}
 
-	const filteredProjectData = projectData.filter((projects: { topics: string | string[] } ) => projects.topics.includes('portfolio-project'))
-    
+	let filteredProjectData: projectData[] = []
+
+	if (projectData.length > 0) {
+		const sortedProjectData = projectData.sort(
+			(a, b) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+		)
+
+		filteredProjectData = sortedProjectData.filter((project) =>
+			project.topics.includes('portfolio-project')
+		)
+	}
+
 	return filteredProjectData
 }
