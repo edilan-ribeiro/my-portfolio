da1f143fccc9fbd7b49775b59262a0ee2b7e6e0a
edilan-ribeiro <eddyoneal2@hotmail.com>
2024-03-10 23:47:01 -0300

    feat: added tech stack from readme into the projects card

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -5,13 +5,12 @@ import { IoIosLink } from 'react-icons/io'
 import { FaGithub } from 'react-icons/fa'
 import projectsStyles from './projects.module.scss'
 import { formatName } from './helpers/formatName'
+import { TechStack } from './TechStack/TechStack'
 
 export async function Projects() {
-	let projectsData = await fetchProjectData()
+	const projectsData = await fetchProjectData()
 
-	//TODO: ACESSAR README DATA PRA PEGAR A TECH COM REGEX NA BADGE
 	//TODO: BOTÃO AO FINAL PARA O LINK GERAL DE REPOS
-	//TODO: ADD +2 REPOS
 
 	return (
 		<div className={projectsStyles.contentContainer}>
@@ -36,15 +35,8 @@ export async function Projects() {
 							<div className={projectsStyles.cardText}>
 								<h3>{formatName(project.name)}</h3>
 								<p>{project.description}</p>
-								<ul className={projectsStyles.cardTech}>
-									<li>tecnologias</li>
-									<li>tecnologias2</li>
-									<li>tecnologias3</li>
-									<li>tecnologias3</li>
-									<li>tecnologias3</li>
-									<li>tecnologias3</li>
-									<li>tecnologias3</li>
-								</ul>
+
+								<TechStack projects={project.name} />
 							</div>
 
 							<div className={projectsStyles.cardLinks}>
