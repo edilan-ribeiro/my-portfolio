530252bdd8dd9001aa79ac1c4baf36b7cd81b49d
edilan-ribeiro <eddyoneal2@hotmail.com>
2024-03-11 18:23:56 -0300

    chore: github access token added to improve rate limit

--- src/services/fetchProjectFile.ts
+++ src/services/fetchProjectFile.ts
@@ -7,10 +7,11 @@ export async function fetchProjectFile(projectName: string): Promise<ProjectFile
 
 	try {
 		const url = `https://api.github.com/repos/edilan-ribeiro/${projectName}/contents/README.md`
+		const token = process.env.GITHUB_TOKEN
 
 		const fileDataCacheTime = 60 * 60 * 24 * 25 // 25 days to revalidate cache
 
-		const getFileData = await fetch(url, { next: { revalidate: fileDataCacheTime } })
+		const getFileData = await fetch(url, { headers: { Authorization: `token ${token}` }, next: { revalidate: fileDataCacheTime } })
 
 		projectFile = await getFileData.json()
 	} catch (error) {
