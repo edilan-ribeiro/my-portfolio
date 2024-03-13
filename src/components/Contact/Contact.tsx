fd842e586de9d25e33d89398f9e66e312ae2ccc9
edilan-ribeiro <eddyoneal2@hotmail.com>
2024-03-13 15:04:16 -0300

    feat: added validation to contact section

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,9 +1,35 @@
 'use client'
+
 import { useForm } from 'react-hook-form'
 import contactStyles from './contact.module.scss'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { SectionTitle } from '../SectionTitle/SectionTitle'
+
+const createContactSchema = z.object({
+	name: z
+		.string()
+		.min(1, { message: '*Por favor, preencha um nome' })
+		.min(4, { message: 'Nome muito curto, utilize ao menos 4 caracteres' })
+		.max(40, { message: 'Nome muito grande, deve ter menos de 40 caracteres' }),
+	email: z
+		.string()
+		.min(1, { message: '*Por favor, informe um email' })
+		.email({ message: 'Formato de email inválido' })
+		.max(130, { message: 'Email muito grande, deve ter menos de 130 caracteres' }),
+	message: z
+		.string()
+		.min(1, { message: '*Por favor, adicione uma mensagem' })
+		.min(10, { message: 'A mensagem deve ter pelo menos 10 caracteres' })
+		.max(400, { message: 'A mensagem pode ter no máximo 400 caracteres' }),
+})
 
 export const Contact = () => {
-	const { register, handleSubmit } = useForm()
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<z.infer<typeof createContactSchema>>({ resolver: zodResolver(createContactSchema) })
 
 	function sendMessage(data: any) {
 		console.log(data)
@@ -11,6 +37,7 @@ export const Contact = () => {
 
 	return (
 		<div className={contactStyles.contentContainer}>
+			<SectionTitle sectionTitle='Contato' />
 			<div className={contactStyles.contact}>
 				<div className={contactStyles.contactContent}>
 					<div className={contactStyles.getInTouch}>
@@ -30,6 +57,11 @@ export const Contact = () => {
 								placeholder='Digite seu nome'
 								{...register('name')}
 							/>
+							{errors.name && (
+								<p className={contactStyles.errorMessage}>
+									{errors.name.message}
+								</p>
+							)}
 						</div>
 						<div className={contactStyles.formFields}>
 							<label htmlFor='email'>Email</label>
@@ -39,14 +71,26 @@ export const Contact = () => {
 								placeholder='Coloque seu email'
 								{...register('email')}
 							/>
+							{errors.email && (
+								<p className={contactStyles.errorMessage}>
+									{errors.email.message}
+								</p>
+							)}
 						</div>
-						<div className={contactStyles.formFields}>
+						<div
+							className={`${contactStyles.formFields} ${contactStyles.messageField}`}
+						>
 							<label htmlFor='message'>Mensagem</label>
 							<textarea
 								id='message'
 								placeholder='Escreva sua mensagem...'
 								{...register('message')}
 							/>
+							{errors.message && (
+								<p className={contactStyles.errorMessage}>
+									{errors.message.message}
+								</p>
+							)}
 						</div>
 					</form>
 					<button type='submit' className={contactStyles.formButton} form='contact-form'>
