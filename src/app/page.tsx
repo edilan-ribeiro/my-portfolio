0d99062f126de88c2ebe3725da307ee8a972fade
edilan-ribeiro <eddyoneal2@hotmail.com>
2024-03-13 15:11:20 -0300

    feat: added sections and its components into the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,32 @@
 import '@/styles/sections/sections.scss'
-import About from '@/components/About/About'
+import { About } from '@/components/About/About'
+import { Header } from '@/components/Header/Header'
+import { Hero } from '@/components/Hero/Hero'
+import { Skills } from '@/components/Skills/Skills'
+import { Projects } from '@/components/Projects/Projects'
+import { Footer } from '@/components/Footer/Footer'
+import { Contact } from '@/components/Contact/Contact'
 
 export default function Home() {
 	return (
 		<main>
-			<section></section>
-			<section className='about'>
+			<Header />
+			<section className='hero'>
+				<Hero />
+			</section>
+			<section className='about' id='about'>
 				<About />
 			</section>
-			<section></section>
-			<section></section>
-			<section></section>
+			<section className='skills' id='skills'>
+				<Skills />
+			</section>
+			<section className='projects' id='projects'>
+				<Projects />
+			</section>
+			<section  id='contact'>
+				<Contact />
+			</section>
+			<Footer />
 		</main>
 	)
 }
