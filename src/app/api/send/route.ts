4ac4a17666f66dbd8cb6891a517f1a8eb1875c17
edilan-ribeiro <eddyoneal2@hotmail.com>
2024-03-16 16:36:45 -0300

    chore: moved format validation to a middleware in order to ensure the data format before the route call

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -2,35 +2,28 @@
 
 import { Resend } from 'resend'
 import { NextRequest, NextResponse } from 'next/server'
-import { contactFormSchema } from '@/lib/contactFormSchema'
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
 export async function POST(req: NextRequest, res: NextResponse) {
 	const formData = await req.json()
 
-	const validate = contactFormSchema.safeParse(formData)
+	const { name, email, message } = formData
 
-	if (!validate.success) {
-		return NextResponse.json({ message: 'Incorrect data format' }, { status: 422 })
-	} else {
-		const { name, email, message } = formData
+	try {
+		const data = await resend.emails.send({
+			from: `delivered@resend.dev`,
+			to: ['edilanbusiness@gmail.com'],
+			subject: 'Contato pelo portfólio',
+			text: `${name} enviou com o email ${email} e a seguinte mensagem: ${message}`,
+		})
 
-		try {
-			const data = await resend.emails.send({
-				from: `delivered@resend.dev`,
-				to: ['edilanbusiness@gmail.com'],
-				subject: 'Contato pelo portfólio',
-				text: `${name} enviou com o email ${email} e a seguinte mensagem: ${message}`,
-			})
-
-			return NextResponse.json({data}, { status: 200 })
-		} catch (error) {
-			console.log(error)
-			return NextResponse.json(
-				{ message: 'There has been an error while sending  the message' },
-				{ status: 500 }
-			)
-		}
+		return NextResponse.json({ data }, { status: 200 })
+	} catch (error) {
+		console.log(error)
+		return NextResponse.json(
+			{ message: 'There has been an error while sending  the message' },
+			{ status: 500 }
+		)
 	}
 }
