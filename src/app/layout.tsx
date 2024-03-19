063602a6e7f836ccb0577954ac41602516410abc
edilan-ribeiro <eddyoneal2@hotmail.com>
2024-03-19 19:37:11 -0300

    feat: added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,16 @@
 import type { Metadata } from 'next'
+
 import { Open_Sans } from 'next/font/google'
 import '@/styles/global.scss'
 
 const openSans = Open_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-	title: 'Edilan Ribeiro - Desenvolvedor Fullstack',
+	title: 'Edilan Ribeiro - Desenvolvedor Full Stack',
 	description: 'Portfolio de Edilan Ribeiro',
+	icons: {
+		icon: '/favicon.svg',
+	},
 }
 
 export default function RootLayout({
