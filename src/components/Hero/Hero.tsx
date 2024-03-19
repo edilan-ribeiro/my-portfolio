b4f15d4c94a1ffeb4985a9c249108082ab97b41b
edilan-ribeiro <eddyoneal2@hotmail.com>
2024-03-19 19:17:40 -0300

    style: added hero section video and animations

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,15 +1,30 @@
 import { Socials } from './Socials/Socials'
 import heroStyles from './hero.module.scss'
+import { FaChevronDown } from 'react-icons/fa'
+import fallbackImage from '@/assets/hero/clouds-bg.jpg'
 
 export const Hero = () => {
 	return (
-		<div className={heroStyles.contentContainer}>
-			<div className={heroStyles.heroContent}>
-				<h1>Edilan Ribeiro</h1>
-				<h2>Desenvolvedor Fullstack</h2>
-
-				<Socials />
+		<section className={heroStyles.hero} id='hero'>
+			<div className={heroStyles.heroBackground}>
+				<video autoPlay muted loop poster={fallbackImage.src}>
+					<source
+						src='https://mylivewallpapers.com/wp-content/uploads/Fantasy/PREVIEW-Watching-the-Universe-Blue.mp4'
+						type='video/mp4'
+					/>
+				</video>
 			</div>
-		</div>
+			<div className={heroStyles.contentContainer}>
+				<div className={heroStyles.heroContent}>
+					<h1>Edilan Ribeiro</h1>
+					<h2>Desenvolvedor Full Stack</h2>
+
+					<Socials />
+				</div>
+			</div>
+			<a href='#about' className={heroStyles.scrollIndicator} aria-label='ir para a próxima sessão'>
+				<FaChevronDown />
+			</a>
+		</section>
 	)
 }
