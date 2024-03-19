803806e95a7b7859d9fe3fdf63513c0ce8a98b9a
edilan-ribeiro <eddyoneal2@hotmail.com>
2024-03-19 19:38:10 -0300

    refactor: moved sections inside the components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import '@/styles/sections/sections.scss'
 import { About } from '@/components/About/About'
 import { Header } from '@/components/Header/Header'
 import { Hero } from '@/components/Hero/Hero'
@@ -11,21 +10,17 @@ export default function Home() {
 	return (
 		<main>
 			<Header />
-			<section className='hero'>
-				<Hero />
-			</section>
-			<section className='about' id='about'>
-				<About />
-			</section>
-			<section className='skills' id='skills'>
-				<Skills />
-			</section>
-			<section className='projects' id='projects'>
-				<Projects />
-			</section>
-			<section  id='contact'>
-				<Contact />
-			</section>
+
+			<Hero />
+
+			<About />
+
+			<Skills />
+
+			<Projects />
+
+			<Contact />
+
 			<Footer />
 		</main>
 	)
