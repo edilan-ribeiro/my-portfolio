c86028a023e00cf14393f9100bebba796b0ccb96
edilan-ribeiro <eddyoneal2@hotmail.com>
2024-04-02 14:00:14 -0300

    feat: added initial mocks configuration

---
+++ src/__tests__/.vitest/mocks/handlers.ts
@@ -0,0 +1,17 @@
+import { HttpResponse, http } from 'msw'
+import mockRepos from './mockData/mockRepos.json'
+import mockFiles from './mockData/mockFiles.json'
+
+export const handlers = [
+	http.get('https://api.github.com/users/edilan-ribeiro/repos', () => {
+		return HttpResponse.json({ mockRepos }, { status: 200 })
+	}),
+
+	http.get('https://api.github.com/repos/edilan-ribeiro/:repoName/contents/README.md', ({ params }) => {
+		const { repoName } = params
+
+		const correctRepository = mockFiles.find((repo) => repo.name === repoName)
+
+		return HttpResponse.json({ correctRepository }, { status: 200 })
+	}),
+]
--- src/__tests__/.vitest/setup.ts
+++ src/__tests__/.vitest/setup.ts
@@ -1,10 +1,13 @@
-import { vi, beforeEach, afterEach } from 'vitest'
+import { vi, beforeEach, afterEach, afterAll, beforeAll } from 'vitest'
 import { setupIntersectionMocking, resetIntersectionMocking } from 'react-intersection-observer/test-utils'
 import { MotionGlobalConfig } from 'framer-motion'
 import { cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/vitest'
+import { server } from './mocks/mockServerSetup'
 
 MotionGlobalConfig.skipAnimations = true
+
+beforeAll(() => server.listen({ onUnhandledRequest: 'warn' }))
 
 beforeEach(() => {
 	setupIntersectionMocking(vi.fn)
@@ -12,5 +15,8 @@ beforeEach(() => {
 
 afterEach(() => {
 	resetIntersectionMocking()
+	server.resetHandlers()
 	cleanup()
 })
+
+afterAll(() => server.close())
